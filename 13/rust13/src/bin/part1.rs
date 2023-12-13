72d225f23ac992e66de67915e2cea7191d3149b9
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2023-12-13 12:06:39 +0100

    day 13 - rust

---
+++ 13/rust13/src/bin/part1.rs
@@ -0,0 +1,117 @@
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("../../../input/part1.txt");
+
+    let answer = process(input);
+
+    println!("Answer {answer}");
+}
+
+fn check_horizontal(v: &[Vec<char>]) -> Option<usize> {
+    let mirror_locations =
+        v.iter()
+            .tuple_windows()
+            .enumerate()
+            .filter_map(
+                |(index, (line_a, line_b))| {
+                    if line_a == line_b {
+                        Some(index)
+                    } else {
+                        None
+                    }
+                },
+            );
+
+    for loc in mirror_locations {
+        let split_a = &v[..(loc + 1)];
+        let split_b = &v[(loc + 1)..];
+
+        let mut broke = false;
+        for (v_a, v_b) in split_a.iter().rev().zip(split_b.iter()) {
+            if v_a != v_b {
+                broke = true;
+                break;
+            }
+        }
+        if !broke {
+            return Some(loc);
+        }
+    }
+
+    None
+}
+
+fn check_vertical(v: &[Vec<char>]) -> Option<usize> {
+    let mut iters = v.iter().map(|line| line.iter()).collect::<Vec<_>>();
+    let transformed = std::iter::from_fn(move || {
+        let mut items = Vec::new();
+
+        for iter in &mut iters {
+            match iter.next() {
+                Some(item) => items.push(*item),
+                None => return None,
+            }
+        }
+
+        Some(items)
+    })
+    .collect::<Vec<_>>();
+
+    check_horizontal(&transformed)
+}
+
+fn process(input: &str) -> usize {
+    let n_line = if cfg!(windows) & !cfg!(test) {
+        "\r\n\r\n"
+    } else {
+        "\n\n"
+    };
+    input
+        .split(n_line)
+        .map(|grid| {
+            let grid = grid
+                .lines()
+                .map(|line| line.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>();
+            let mut answer = 0;
+            let hor = check_horizontal(&grid);
+            let ver = check_vertical(&grid);
+
+            if let Some(hor) = hor {
+                answer += (hor + 1) * 100;
+            }
+            if let Some(ver) = ver {
+                answer += ver + 1;
+            }
+
+            answer
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn d13_part_1_is_correct() {
+        let input = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+        assert_eq!(process(input), 405);
+    }
+}
