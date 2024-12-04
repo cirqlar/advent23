b21600f8f30f6cedf7427b961206080fdef496cf
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2024-12-04 14:49:07 +0100

    test: bench without input? include macro shouldn't be adding anything to the bench so this shouldn't make a difference

--- 2024/04/rust04/benches/main.rs
+++ 2024/04/rust04/benches/main.rs
@@ -31,3 +31,31 @@ fn part2_flatpar() -> i32 {
 
     part2::process_flatpar(input)
 }
+
+#[divan::bench()]
+fn part1_bencher(bencher: divan::Bencher) {
+    let input = include_bytes!("../../input/part1.txt");
+
+    bencher.bench_local(|| part1::process(input));
+}
+
+#[divan::bench()]
+fn part1_flatpar_bencher(bencher: divan::Bencher) {
+    let input = include_bytes!("../../input/part1.txt");
+
+    bencher.bench_local(|| part1::process_flatpar(input));
+}
+
+#[divan::bench()]
+fn part2_bencher(bencher: divan::Bencher) {
+    let input = include_bytes!("../../input/part1.txt");
+
+    bencher.bench_local(|| part2::process(input));
+}
+
+#[divan::bench()]
+fn part2_flatpar_bencher(bencher: divan::Bencher) {
+    let input = include_bytes!("../../input/part1.txt");
+
+    bencher.bench_local(|| part2::process_flatpar(input));
+}
